35d586667027af243f194dcae5b5a10ec0ab1520
Quoc Huy <quochuy.ws@gmail.com>
2024-08-28 21:32:05 +0700

    UI for waiting room

---
+++ Assets/PlayerInWaitingRoom.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerInWaitingRoom : MonoBehaviour
+{
+    public Object isHost;
+    public TMP_Text playerName;
+    public Object ready;
+
+    public Object playerCharacter;
+}
