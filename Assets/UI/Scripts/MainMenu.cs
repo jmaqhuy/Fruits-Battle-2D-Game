d1e937c0257e30057540b55dcde26739d9dd7466
Quoc Huy <quochuy.ws@gmail.com>
2024-09-23 16:17:40 +0700

    long time no see

--- Assets/UI/Scripts/MainMenu.cs
+++ Assets/UI/Scripts/MainMenu.cs
@@ -21,4 +21,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Shop");
     }
+    
+    public void SelectPlayMode()
+    {
+        SceneManager.LoadScene("Select Play Mode");
+    }
 }
