f0e2f828fd3f36d97591109c907de8e19e54989d
Quoc Huy <quochuy.ws@gmail.com>
2024-11-13 17:37:05 +0700

    Simple UserProfile UI

---
+++ Assets/UI/Scripts/PlayerInWaitingRoom.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PlayerInWaitingRoom : MonoBehaviour
+{
+    public TextMeshProUGUI PlayerName;
+    public GameObject isHost;
+    public GameObject isReady;
+    public GameObject character;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
