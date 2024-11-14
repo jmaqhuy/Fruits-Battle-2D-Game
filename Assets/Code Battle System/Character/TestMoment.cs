296ee2441e7867d8ee638913202a80f0291d24c5
Quoc Huy <quochuy.ws@gmail.com>
2024-11-14 17:12:45 +0700

    animation for moving and shooting

--- Assets/Code Battle System/Character/TestMoment.cs
+++ Assets/Code Battle System/Character/TestMoment.cs
@@ -7,6 +7,7 @@ public class TestMoment : MonoBehaviour
     public float speedMovement = 5f;
     public Rigidbody2D rb;
     private Vector2 _movement;
+    public Animator animator;
 
     public GameObject Player;
     // Start is called before the first frame update
@@ -24,12 +25,28 @@ public class TestMoment : MonoBehaviour
         _movement.x = Input.GetAxisRaw("Horizontal"); 
         if (_movement.x < 0) 
         { 
+            animator.SetBool("moving", true);
             Player.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
                 
         }
-        if (_movement.x > 0) 
+        else if (_movement.x > 0) 
         { 
+            animator.SetBool("moving", true);
             Player.transform.rotation = Quaternion.Euler(0f, 0f, 0f); 
         }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("shooting", true);
+        }
+        else
+        {
+            animator.SetBool("shooting", false);
+        }
+        
     }
 }
