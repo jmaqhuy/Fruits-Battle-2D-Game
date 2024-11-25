a0ac1c8b3ec0d6d4ece2bbb57c970d528040befe
Quoc Huy <quochuy.ws@gmail.com>
2024-11-26 00:56:43 +0700

    Redesign Waiting Room: Add firned list on right hand side

---
+++ Assets/FriendPanel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class FriendPanel : MonoBehaviour
+{
+    public Button FriendAvatar;
+    public TextMeshProUGUI FriendName;
+    public GameObject OnlineStatusPanel;
+    public Button InviteFriendButton;
+}
